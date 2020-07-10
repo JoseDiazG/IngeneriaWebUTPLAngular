f7046be015059082736204b830686677639bcf43
JoseDiazG <josethp1@hotmail.com>
2020-07-10 15:16:27 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cotizador';
+  title = 'cotizador Web';
 }
+
+var div = document.getElementById("inputEmail4");
+console.log(div);
+
+
